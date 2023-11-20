5a1e1f7b8d727452266445457da127c92e113b26
dungkrs20 <dungkrs20@gmail.com>
2023-11-20 08:05:39 +0700

    update User

--- QuanLyCuaHangSach/QuanLyCuaHangSach/User.cs
+++ QuanLyCuaHangSach/QuanLyCuaHangSach/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,43 @@ namespace QuanLyCuaHangSach
         public User()
         {
             InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("server=ADMIN\\SLTP;" + "uid=tk1;pwd=1;" + "database = QL_Cua_Hang_Sach");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+
+        private void User_Load(object sender, EventArgs e)
+        {
+            LoadDataGridView();
+            save.Enabled = false;
+            edit.Enabled = false;
+            delete.Enabled = false;
+            reset.Enabled = false;
+        }
+
+        private void LoadDataGridView()
+        {
+            con.Open();
+            DataTable tblUser = new DataTable();
+            adapt = new SqlDataAdapter("select * from tblUser", con);
+            adapt.Fill(tblUser);
+            user_dgv.DataSource = tblUser;
+            user_dgv.Columns[0].HeaderText = "ID";
+            user_dgv.Columns[1].HeaderText = "User name";
+            user_dgv.Columns[2].HeaderText = "Phone";
+            user_dgv.Columns[3].HeaderText = "Address";
+            user_dgv.Columns[4].HeaderText = "Password";
+            user_dgv.Columns[0].Width = 100;
+            user_dgv.Columns[1].Width = 150;
+            user_dgv.Columns[2].Width = 150;
+            user_dgv.Columns[3].Width = 200;
+            user_dgv.Columns[4].Width = 200;
+            user_dgv.AllowUserToAddRows = false;
+            user_dgv.EditMode = DataGridViewEditMode.EditProgrammatically;
+            con.Close();
+
+            
         }
     }
 }
